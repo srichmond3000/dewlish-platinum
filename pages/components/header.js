71709b30638207034a61edc7c75d10b57339af9b
srichmond3000 <srichmond3000@googlemail.com>
2021-07-18 07:35:53 +0100

    Work on header with union jack

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,10 +1,18 @@
+import Image from 'next/image';
 import Nav from './nav/nav';
 import styles from './header.module.scss';
 
 const Header = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.image}>Image</div>
+      <div className={styles.image}>
+        <Image
+          src='/images/union-jack.jpg'
+          alt='Union Jack'
+          width='150'
+          height='87'
+        />
+      </div>
       <div className={styles.title}>
         <h1>Header</h1>
       </div>
