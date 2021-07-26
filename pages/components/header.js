dd3a0ec728da00614e5ec216e5c698f6d05140c1
srichmond3000 <srichmond3000@googlemail.com>
2021-07-26 20:53:38 +0100

    Try dropping height attr as well

--- pages/components/header.js
+++ pages/components/header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.image}>
-        <Image src='/images/union-jack.jpg' alt='Union Jack' width={150} />
+        <Image src='/images/union-jack.jpg' alt='Union Jack' />
       </div>
       <div className={styles.title}>
         <h1>The Queen&apos;s Platinum Jubilee 2022</h1>
