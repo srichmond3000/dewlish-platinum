e0df801b4ef4ae52ce1df31260711753f02c5823
srichmond3000 <srichmond3000@googlemail.com>
2021-07-30 05:34:09 +0100

    Changes to contact form

--- pages/contact.js
+++ pages/contact.js
@@ -5,31 +5,39 @@ const Contact = () => {
   return (
     <Layout title='Dewlish Platinum - Contact'>
       <p>Enter your details and message and we will get back to you.</p>
-      <form name='contact' className={styles.form} netlify>
-        <label htmlFor='name' className={styles.label}>
-          Name
-        </label>
-        <input id='name' type='text' name='name' className={styles.input} />
 
-        <label htmlFor='email' className={styles.label}>
-          Email
-        </label>
-        <input id='email' type='email' name='email' className={styles.input} />
+      <form name='contact' method='POST' data-netlify='true'>
+        <div className={styles.form}>
+          <label htmlFor='name' className={styles.label}>
+            Name
+          </label>
+          <input id='name' type='text' name='name' className={styles.input} />
 
-        <label htmlFor='message' className={styles.label}>
-          Message
-        </label>
-        <textarea
-          id='message'
-          name='message'
-          rows='4'
-          cols='50'
-          className={styles.input}
-        />
+          <label htmlFor='email' className={styles.label}>
+            Email
+          </label>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            className={styles.input}
+          />
 
-        <button type='submit' className={styles.button}>
-          Send
-        </button>
+          <label htmlFor='message' className={styles.label}>
+            Message
+          </label>
+          <textarea
+            id='message'
+            name='message'
+            rows='4'
+            cols='50'
+            className={styles.input}
+          />
+
+          <button type='submit' className={styles.button}>
+            Send
+          </button>
+        </div>
       </form>
     </Layout>
   );
