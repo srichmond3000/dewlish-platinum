8b39d5211525fa04edb848679af6ef062235f428
srichmond3000 <srichmond3000@googlemail.com>
2021-08-02 22:03:19 +0100

    Added facebook link in footer

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -1,7 +1,24 @@
+import Image from 'next/image';
 import styles from './footer.module.scss';
 
 const Footer = () => {
-  return <p className={styles.footer}>&copy; Dewlish Platinum.org 2021</p>;
+  return (
+    <div className={styles.footerContainer}>
+      <span className={styles.copy}>&copy; Dewlish Platinum 2021</span>
+
+      <a
+        href='https://www.facebook.com/Dewlish-VillageDorset-984377341722347'
+        className={styles.facebookLink}
+      >
+        <Image
+          src='/images/f_logo_RGB-Blue_58.png'
+          alt='Link to facebook'
+          width={24}
+          height={24}
+        />
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
