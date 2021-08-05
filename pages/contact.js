b79b550abdfa90748050f9a91ab2b49e5ae3fef6
srichmond3000 <srichmond3000@googlemail.com>
2021-08-05 06:10:45 +0100

    Make contact form stateful with validation

--- pages/contact.js
+++ pages/contact.js
@@ -1,25 +1,94 @@
+import { useState, useEffect } from 'react';
 import Layout from './components/layout';
 import styles from './contact.module.scss';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [errors, setErrors] = useState({});
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && submitted) {
+      fetch('/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-wwww-form-urlencoded',
+        },
+        body: encode({ 'form-name': 'contact', ...formData }),
+      })
+        .then(() => alert('Success'))
+        .then(() => setSubmitted(false))
+        .then(() => setFormData({ name: '', email: '', message: '' }))
+        .catch((error) => alert(error));
+    }
+  }, [errors, formData, submitted]);
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&');
+  };
+
+  const validate = (formData) => {
+    let formErrors = {};
+
+    if (!formData.name) {
+      formErrors.name = 'Name required';
+    }
+    if (!formData.email) {
+      formErrors.email = 'Email required';
+    }
+    if (!formData.message) {
+      formErrors.message = 'Message required';
+    }
+
+    return formErrors;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    setErrors(validate(formData));
+    setSubmitted(true);
+    e.preventDefault();
+  };
+
   return (
     <Layout title='Dewlish Platinum - Contact'>
       <p>Enter your details and message and we will get back to you.</p>
+      {/* This form is just to allow the Netlify bot to find the netlfiy attribute */}
+      <form name='contact' netlify netlify-honeypot='bot-field' hidden>
+        <input type='text' name='name' />
+        <input type='email' name='email' />
+        <textarea name='message'></textarea>
+      </form>
 
-      <form
-        name='contact'
-        method='POST'
-        data-netlify='true'
-        netlify-honeypot='bot-field'
-        action='/thankyou'
-      >
+      <form name='contact' action='/thankyou' onSubmit={handleSubmit}>
         <input type='hidden' name='form-name' value='contact' />
         <input type='text' name='bot-field' className={styles.hidden} />
         <div className={styles.form}>
           <label htmlFor='name' className={styles.label}>
             Name
           </label>
-          <input id='name' type='text' name='name' className={styles.input} />
+          <input
+            id='name'
+            type='text'
+            name='name'
+            className={styles.input}
+            value={formData.name}
+            onChange={handleChange}
+          />
+          {errors.name && <p>{errors.name}</p>}
 
           <label htmlFor='email' className={styles.label}>
             Email
@@ -29,7 +98,10 @@ const Contact = () => {
             type='email'
             name='email'
             className={styles.input}
+            value={formData.email}
+            onChange={handleChange}
           />
+          {errors.email && <p>{errors.email}</p>}
 
           <label htmlFor='message' className={styles.label}>
             Message
@@ -40,7 +112,10 @@ const Contact = () => {
             rows='4'
             cols='50'
             className={styles.input}
+            value={formData.message}
+            onChange={handleChange}
           />
+          {errors.message && <p>{errors.message}</p>}
 
           <button type='submit' className={styles.button}>
             Send
