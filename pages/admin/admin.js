d0e8c8b8f5db0b314a0b0743af55540bc40f0c17
srichmond3000 <srichmond3000@googlemail.com>
2021-08-12 08:26:19 +0100

    Minor change

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -9,7 +9,7 @@ const Admin = () => {
     <Layout title='Admin'>
       <h2>Admin area</h2>
       <a href='https://ucarecdn.com/5811785a-16eb-455a-9ad2-8e3cdaa5c282/PlatinumJubileeCommittee_MeetingNotes_20200803.pdf'>
-        Meeting notes - 3rd Aug 2021
+        Meeting notes for 3rd Aug 2021
       </a>
       <br />
       <a href='https://ucarecdn.com/b7d02f18-9885-4cb9-8505-7b53f624bd7f/Grantapplication.docx'>
