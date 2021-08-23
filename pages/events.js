92ae3ed42f7fefc080e9014b028a116fdeff230e
srichmond3000 <srichmond3000@googlemail.com>
2021-08-23 17:46:29 +0100

    Add tea on the lawn event

--- pages/events.js
+++ pages/events.js
@@ -4,7 +4,17 @@ import Layout from './components/layout';
 const Events = () => {
   return (
     <Layout title='Dewlish Platinum - Events'>
-      <h2>Family Barbecue Sat 31st July 2021</h2>
+      <h2>Upcoming events</h2>
+      <h3>Tea on the Lawn Sun 5th Aug 2021</h3>
+      <p>
+        Traditional tea with cream or savoury delights from 3.00 to 5.00pm. To
+        be held at Akerian, Dewlish courtesy of Albert and Nonie Ross. All
+        welcome, please come and enjoy yourselves.
+      </p>
+      <p>Sorry, please no dogs.</p>
+      <hr />
+      <h2>Past events</h2>
+      <h3>Family Barbecue Sat 31st July 2021</h3>
       <p>
         Our first event was a family barbecue outside the village hall on Sat
         31st July. There was a great turnout of people to enjoy the burgers and
