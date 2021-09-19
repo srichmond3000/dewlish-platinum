7d740b93485827b431b30e74acfe103e56e8f629
srichmond3000 <srichmond3000@googlemail.com>
2021-09-19 07:40:08 +0100

    Add tea on the lawn event write up

--- pages/events.js
+++ pages/events.js
@@ -5,15 +5,36 @@ const Events = () => {
   return (
     <Layout title='Dewlish Platinum - Events'>
       <h2>Upcoming events</h2>
+      <h3>Bonfire Night Sat 6th Nov 2021</h3>
+      <p>Enjoy the traditional Dewlish bonfire night.</p>
+      <h3>Barn Dance Sat 27th Nov 2021</h3>
+      <p>Join us at Deverell Farm, Milborne St Andrew for a Barn Dance.</p>
+      <h3>Auction of Promises Sat 12th Feb 2022</h3>
+      <p>Details to follow</p>
+      <hr />
+
+      <h2>Past events</h2>
       <h3>Tea on the Lawn Sun 5th Sept 2021</h3>
       <p>
-        Traditional tea with cream or savoury delights from 3.00 to 5.00pm. To
-        be held at Akerian, Dewlish courtesy of Albert and Nonie Ross. All
-        welcome, please come and enjoy yourselves.
+        The great and the good from our village - which explains why there were
+        so many there! - came to the picturesque gardens of Albert &amp; Nonie
+        Ross for a full blown cream tea with scones, clotted cream, strawberry
+        jam &amp; a glass of wine. All this in aid of the swelling fund for the
+        Queen&apos;s Platinum Jubilee celebrations in 2022. Glorious weather
+        weather and a chance to spend time with friends old and new while
+        relaxing in sylvan surrounds and contributing to such a good cause - why
+        not! This was the latest in a string of happenings scheduled to lead up
+        to the events themselves and which are indicative of the strong bonds of
+        community which support such efforts. Our thanks to the many who helped
+        make the day a resounding success!
       </p>
-      <p>Sorry, please no dogs.</p>
-      <hr />
-      <h2>Past events</h2>
+      <Image
+        src='/images/tea-party.jpg'
+        alt='People at the afternoon tea'
+        width={1600}
+        height={900}
+      />
+
       <h3>Family Barbecue Sat 31st July 2021</h3>
       <p>
         Our first event was a family barbecue outside the village hall on Sat
