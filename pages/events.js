5208ec10150c7b6c29847898bb97099e09faefaa
srichmond3000 <srichmond3000@googlemail.com>
2021-09-21 06:10:37 +0100

    Improve styling

--- pages/events.js
+++ pages/events.js
@@ -5,16 +5,24 @@ const Events = () => {
   return (
     <Layout title='Dewlish Platinum - Events'>
       <h2>Upcoming events</h2>
-      <h3>Bonfire Night Sat 6th Nov 2021</h3>
-      <p>Enjoy the traditional Dewlish bonfire night.</p>
-      <h3>Barn Dance Sat 27th Nov 2021</h3>
-      <p>Join us at Deverell Farm, Milborne St Andrew for a Barn Dance.</p>
-      <h3>Auction of Promises Sat 12th Feb 2022</h3>
-      <p>Details to follow</p>
+      <section className='event'>
+        <h3>Bonfire Night Sat 6th Nov 2021</h3>
+        <p>Enjoy the traditional Dewlish bonfire night.</p>
+      </section>
+
+      <section className='event'>
+        <h3>Barn Dance Sat 27th Nov 2021</h3>
+        <p>Join us at Deverell Farm, Milborne St Andrew for a Barn Dance.</p>
+      </section>
+
+      <section className='event'>
+        <h3>Auction of Promises Sat 12th Feb 2022</h3>
+        <p>Details to follow</p>
+      </section>
       <hr />
 
       <h2>Past events</h2>
-      <section>
+      <section className='event'>
         <h3>Tea on the Lawn Sun 5th Sept 2021</h3>
         <p>
           The great and the good from our village - which explains why there
@@ -37,27 +45,29 @@ const Events = () => {
         />
       </section>
 
-      <h3>Family Barbecue Sat 31st July 2021</h3>
-      <p>
-        Our first event was a family barbecue outside the village hall on Sat
-        31st July. There was a great turnout of people to enjoy the burgers and
-        hotdogs served up by Andy, Chris, Ron and Carole. Many thanks to Phil
-        and Katy for running the raffle. Plus not to forget Andy running the
-        game of &apos;heads and tails&apos; at the end.
-      </p>
-      <Image
-        src='/images/Optimized-barbecue.jpg'
-        alt='People sat outside village hall at barbecue'
-        width={500}
-        height={375}
-      />
-      <p>The milk tanker squeezed through...just!</p>
-      <Image
-        src='/images/Optimized-barbecue-milk-tanker.jpg'
-        alt='Milk tanker squeezing past barbecue'
-        width={500}
-        height={375}
-      />
+      <section className='event'>
+        <h3>Family Barbecue Sat 31st July 2021</h3>
+        <p>
+          Our first event was a family barbecue outside the village hall on Sat
+          31st July. There was a great turnout of people to enjoy the burgers
+          and hotdogs served up by Andy, Chris, Ron and Carole. Many thanks to
+          Phil and Katy for running the raffle. Plus not to forget Andy running
+          the game of &apos;heads and tails&apos; at the end.
+        </p>
+        <Image
+          src='/images/Optimized-barbecue.jpg'
+          alt='People sat outside village hall at barbecue'
+          width={500}
+          height={375}
+        />
+        <p>The milk tanker squeezed through...just!</p>
+        <Image
+          src='/images/Optimized-barbecue-milk-tanker.jpg'
+          alt='Milk tanker squeezing past barbecue'
+          width={500}
+          height={375}
+        />
+      </section>
     </Layout>
   );
 };
