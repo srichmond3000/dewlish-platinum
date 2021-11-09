8d5263e5fec27c21a7d66e93dd76fc0bf460c3fe
srichmond3000 <srichmond3000@googlemail.com>
2021-11-09 08:15:37 +0000

    Add meeting minutes

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -9,6 +9,11 @@ const Admin = () => {
     <Layout title='Admin'>
       <h2>Admin area</h2>
       <h3>Minutes of meetings</h3>
+      <a href='https://ucarecdn.com/1cd69ac8-852c-472e-9ecd-96a2e858bd2d/PlatinumJubileeCommittee_MeetingNotes_20211019.pdf'>
+        19th Oct 2021
+      </a>
+      <br />
+      <br />
       <a href='https://ucarecdn.com/11ddc131-6590-46c0-8950-1e733174bf3e/CommitteeMeetingMinutes24thAugust2021.pdf'>
         24th Aug 2021
       </a>
