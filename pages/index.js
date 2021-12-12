9aa9a0d4eb6ea5dbb2f5debeb2956cb6f412433f
srichmond3000 <srichmond3000@googlemail.com>
2021-12-12 21:37:42 +0000

    Update wording

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,16 @@ const Home = () => {
   return (
     <Layout title='Dewlish Platinum - Home'>
       <p>
-        The village of Dewlish is organising a series of fund raising events
-        through 2021-2022 to raise money to celebrate the Queen&apos;s Platinum
-        Jubilee over the extended bank holiday weekend of Thurs 2nd to Sun 5th
-        June 2022.
+        2022 sees the Queen&apos;s Platinum Jubilee which will be celebrated
+        nationally with an extended bank holiday weekend from Thurs 2nd to Sun
+        5th June. The village of Dewlish is planning to organise several events
+        to mark the occasion and also provide an opportunity for the community
+        to come together again after the disruption caused by Covid. The main
+        events will be the lighting of a beacon on Whitelands Down above the
+        village on Thurs 2nd: part of the national network of beacons, and a
+        dance &amp; barbecue on Sat 4th June. To help fund this we will be
+        holding a series of events in the run up to the Platinum Jubilee
+        weekend, which will be fun social occasions also.
       </p>
       <Image
         src='/images/village-hall-new.jpeg'
@@ -21,17 +27,24 @@ const Home = () => {
       <br />
       <section>
         <h2>Events</h2>
-
+        <p>
+          We&apos;re looking forward to an Auction of Promises and a Barn Dance
+          in the New Year. Previous events include the Dewlish Bonfire Night,
+          Tea on the Lawn and a family barbecue.
+        </p>
         <p>
           See the{' '}
           <ActiveLink href='/events'>
             <span>Events</span>
           </ActiveLink>{' '}
-          page for details of our upcoming and past events.
+          page for further details of our upcoming and past events.
         </p>
+      </section>
+      <section>
+        <h2>Dewlish</h2>
         <p>
-          Dewlish is a thriving small village in the heart of Dorset. Find out
-          more about it{' '}
+          Dewlish is a small village in the heart of Dorset with a thriving
+          community. Find out more about it{' '}
           <Link href='/dewlish'>
             <a>here</a>
           </Link>
