a53168e6e8d8b8683b432808648282c31000ef52
srichmond3000 <srichmond3000@googlemail.com>
2022-03-01 21:13:28 +0000

    Add poster to home page

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,21 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Layout from './components/layout';
 import ActiveLink from './components/nav/activeLink';
+import styles from './index.module.scss';
 
 const Home = () => {
   return (
     <Layout title='Dewlish Platinum - Home'>
+      <div className={styles.posterContainer}>
+        <div className={styles.details}>Text</div>
+        <Image
+          src='/images/barnDancePoster.jpg'
+          alt='Barn Dance poster'
+          width={1200}
+          height={1697}
+          className={styles.poster}
+        />
+      </div>
       <p>
         This year sees the Queen&apos;s Platinum Jubilee which will be
         celebrated nationally with an extended bank holiday weekend from Thurs
