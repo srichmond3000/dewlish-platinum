0c9f28ef9037c07475b5ebac91c8b151362c5e6a
srichmond3000 <srichmond3000@googlemail.com>
2022-03-04 06:28:06 +0000

    Wording changes

--- pages/components/barnDanceDetails.js
+++ pages/components/barnDanceDetails.js
@@ -1,11 +1,16 @@
-const BarnDanceDetails = ({onlineMessage}) => (
+import Link from 'next/link';
+
+const BarnDanceDetails = () => (
   <>
+    <h2>Barn Dance Sat 16th April</h2>
     <p>
-      Come and join us at Deverel Farm, Milborne St Andrew, DT11 0HX on Sat 16th
-      April 2022 for an evening of dance, drinks, eats and merriment. Tickets at
-      £10 available from:
+      Come and join us at Deverel Farm, Milborne St Andrew, DT11 0HX for an
+      evening of dance, drinks, eats and merriment. Tickets at £10 available
+      from:
       <ul>
-        <li>Online (plus 84p booking fee){onlineMessage}</li>
+        <li>
+          <Link href='/events/barndance'>Online</Link>
+        </li>
         <li>
           The Oak Pub, Dewlish <a href='#'>map</a>
         </li>
@@ -13,19 +18,6 @@ const BarnDanceDetails = ({onlineMessage}) => (
           Milborne St Andrew Post Office <a href='#'>map</a>
         </li>
       </ul>
-    </p>
-    <p>
-      With ample car parking on site, doors and bar open at 7:30pm and dancing
-      starting around 8pm until 11:30pm. Music from Dorset’s liveliest party
-      band <a href='https://www.blacksheepband.co.uk/'>Black Sheep</a>, called
-      by Big Al.
-    </p>
-    <p>
-      Why not enjoy a burger or hot dog along with a drink from our licensed
-      bar?
-    </p>
-    <p>
-      Proceeds from this event will go to the Dewlish Platinum Jubilee fund.
     </p>
   </>
 );
--- pages/events/barndance.js
+++ pages/events/barndance.js
@@ -14,7 +14,32 @@ const BarnDance = () => {
     <Layout title='Barn Dance'>
       <h2>Barn Dance Sat 27th Nov 2021</h2>
       <div id='embedTS_JFHDE' style={{ width: '100%' }}></div>
-      <BarnDanceDetails onlineMessage={' (see above)'} />
+      <p>
+        Come and join us at Deverel Farm, Milborne St Andrew, DT11 0HX on Sat
+        16th April 2022 for an evening of dance, drinks, eats and merriment.
+        Tickets at £10 also available from:
+        <ul>
+          <li>
+            The Oak Pub, Dewlish <a href='#'>map</a>
+          </li>
+          <li>
+            Milborne St Andrew Post Office <a href='#'>map</a>
+          </li>
+        </ul>
+      </p>
+      <p>
+        With ample car parking on site, doors and bar open at 7:30pm and dancing
+        starting around 8pm until 11:30pm. Music from Dorset’s liveliest party
+        band <a href='https://www.blacksheepband.co.uk/'>Black Sheep</a>, called
+        by Big Al.
+      </p>
+      <p>
+        Why not enjoy a burger or hot dog along with a drink from our licensed
+        bar?
+      </p>
+      <p>
+        Proceeds from this event will go to the Dewlish Platinum Jubilee fund.
+      </p>
     </Layout>
   );
 };
